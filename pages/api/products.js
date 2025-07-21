327e75b54feceef0c88cd205c345fb7c085964bb
Federico Bojaminsky <federico.bojaminsky@openit.com.ar>
2025-07-21 09:54:32 -0300

    categories commit

--- pages/api/products.js
+++ pages/api/products.js
@@ -16,6 +16,15 @@ export default async function handle(req, res) {
         res.json(productDoc);
     }
 
+    if (method === 'GET') {
+      if(req.query?.id)  {
+        res.json(await Product.findById(req.query.id));
+      }else {
+        res.json(await Product.find());
+    }
+
+    }
+
 if (method === 'DELETE') {
     if(req.query.id) {
       await Project.deleteOne({_id: req.query.id})
