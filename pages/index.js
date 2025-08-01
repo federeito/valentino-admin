0e844df17d2f1d3bb809c7b998eca6f7fbf611a7
Federico Bojaminsky <federico.bojaminsky@openit.com.ar>
2025-08-01 12:26:42 -0300

    commit_link tienda

--- pages/index.js
+++ pages/index.js
@@ -42,9 +42,9 @@ export default function Home() {
                 </svg>
               </Link>
 
-              <button
+              <Link
                 className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-orange-200 px-5 py-3 text-orange-500 transition hover:bg-orange-50 hover:text-orange-700 focus:outline-none focus:ring"
-                type="button"
+                href={"https://valentino-frontend.vercel.app"}
               >
                 <span className="text-md font-medium"> Ver Tienda </span>
 
@@ -52,7 +52,7 @@ export default function Home() {
                   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                 </svg>
 
-              </button>
+              </Link>
             </div>
           </div>
         </div>
